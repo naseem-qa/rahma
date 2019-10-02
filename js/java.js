92eefec7dccb8bb4bd96d92ff14737d162fc5bcb
naseem <izzatnaseem30@gmail.com>
2019-10-02 23:56:37 +0300

    fine

---
+++ js/java.js
@@ -0,0 +1,14 @@
+'use script'
+var naseem = function() {
+
+    var hima;
+    var gh = prompt("How R U ?");
+    if (gh == 'Good' ) {
+        gh = "Hello!";
+    } else if (gh == 'Bad') {
+        gh = "Bad For Me!";
+    }
+return gh;
+}
+
+
