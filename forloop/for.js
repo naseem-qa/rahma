424a40893eb0427f6031a91bf45715441a13e624
naseem <izzatnaseem30@gmail.com>
2019-10-03 14:15:32 +0300

    offff

---
+++ forloop/for.js
@@ -0,0 +1,15 @@
+
+var izzat= function() {
+    var text = "";
+    var gh = prompt('I am')
+    var text = "";
+ var i;
+ for (i = 0; i < 2; i++) {
+   if (i == female  ){ 
+       'Holle Ms'
+   }
+   else if ( i == male){
+       'Holle  Mr'
+   } 
+    text += 'I am' + i + "<br>"; 
+ }
--- js/java.js
+++ js/java.js
@@ -1,3 +1,5 @@
+/* 'use strict ;' */
+
 var naseem = function() {
     var gh = prompt('How R U ?');
     var hima;
@@ -13,6 +15,15 @@ while ( gh !== 'good' && gh !== 'bad' )
 return gh;
 }
 
+var time = prompt('Type number:');
 
+function myFunction1(){
+var i;
+for (i = 1; i < 10; i++) {
 
+ var text = text + 'Type number:' + i + "<br>";
+}
+return text;
+
+}
 
